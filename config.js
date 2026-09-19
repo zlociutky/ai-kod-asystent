1e3445576791c98a7f1e701323349ef98d6e6e59
zlociutky <galkakacper5@gmail.com>
2026-09-19 21:09:45 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://wawahxqkukezpplorlzy.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_W_qmFd1FkmHkH4oFbD8NyA_U1rnmzqz";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
